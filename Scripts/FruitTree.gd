f5916eb8faa6420f4a6d79cf6a4e00f17f4f315e
Tchlok <tchlockdev@gmail.com>
2026-03-04 02:00:17 +0100

    leaf particle and texture changes

--- Scripts/FruitTree.gd
+++ Scripts/FruitTree.gd
@@ -9,11 +9,17 @@ extends Node2D
 @export var leafPacked : PackedScene
 
 @export var tog : SmoothToggle
+@export var parPacked : PackedScene
 
 func _enter_tree():
 	pass
 
 func _ready():
+	var par : CPUParticles2D = parPacked.instantiate()
+	par.rotation_degrees=-90
+	par.modulate=Persistent.c.foliage()
+	add_child(par)
+		
 	var selectedTrunkIdx : int
 	selectedTrunkIdx = randi_range(0,trunkHolder.get_child_count()-1)
 	var selectedTrunk : Sprite2D  = trunkHolder.get_child(selectedTrunkIdx)
